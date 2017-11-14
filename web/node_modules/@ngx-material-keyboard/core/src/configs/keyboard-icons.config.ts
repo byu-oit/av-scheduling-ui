140d891371909017fe254bc658f9cc3a3a670d04
TMP <tmp>
2017-11-14 15:29:58 -0700

    Adding ngx-keyboard

---
+++ web/node_modules/@ngx-material-keyboard/core/src/configs/keyboard-icons.config.ts
@@ -0,0 +1,16 @@
+import { InjectionToken } from '@angular/core';
+
+interface IKeyboardIcons {
+  [key: string]: string;
+}
+
+const MD_KEYBOARD_ICONS = new InjectionToken<IKeyboardIcons>('keyboard-icons.config');
+const keyboardIcons: IKeyboardIcons = {
+  'bksp': 'keyboard_backspace',
+  'caps': 'keyboard_capslock',
+  'enter': 'keyboard_return',
+  'space': '',
+  'tab': 'keyboard_tab'
+};
+
+export { IKeyboardIcons, MD_KEYBOARD_ICONS, keyboardIcons };
